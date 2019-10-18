a0443cf22027762aedcf006b48477f20379c7501
(s) Andrew Bellas <andrew.bellas@students.plymouth.ac.uk>
2019-10-18 11:43:49 +0100

    Added test to identify if a lecturer is assigned to a module

--- TDDWeek04/DegreeWebApp.js
+++ TDDWeek04/DegreeWebApp.js
@@ -24,7 +24,7 @@ class Student{
     
     registerForModule(modName){
         var module = new Module(modName);
-        
+        module.currentEnrol+= 1;
     }
 }
 
@@ -32,9 +32,12 @@ class Lecturer{
     name = "no name";
     numberOfModules = 0;
 
-    constructor(name, numberOfModules){
+    constructor(name){
         this.name = name;
-        this.numberOfModules = numberOfModules;
+    }
+
+    assignToModule(module){
+        module.lecturer = this.name;
     }
 
     changeModules(i){
@@ -46,9 +49,14 @@ class Module{
     name = "null";
     currentEnrol = 0;
     lecturer = "null";
+    studentSet = new Set();
 
     constructor(name){
         this.name = name;
+    }
+
+    enroll(student){
+        this.studentSet.add(student);
     }
 
 }
@@ -57,5 +65,11 @@ suite("JavaScript tests", function() {
     test("Testing if the progress function works", function() {
         var studentOne = new Student("Andrew Bellas", 0);
         chai.assert.equal(studentOne.progress(), 1, "The student stage is 1.");
+    })
+    test("Testing whether lecturers are correctly assigned to the modules.", function(){
+        var lecturerOne = new Lecturer("Mike Truk", 0);
+        var moduleOne = new Module("SOFT355");
+        lecturerOne.assignToModule(moduleOne);
+        chai.assert.equal(moduleOne.lecturer, "Mike Truk", 'Lecturer is correctly assigned.');
     })
 });
